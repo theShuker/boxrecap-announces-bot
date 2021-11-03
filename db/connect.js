fe92d128921441db8b632838a081602fba9b2601
tehShuker <shuker.96@gmail.com>
2021-11-03 14:20:34 +0300

    cool

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL + (process.env.NODE_ENV ? '?ssl=true' : ''), {
     dialect: 'postgres',
     logging: false
 })
