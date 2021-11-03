28c5d98b3fa6833d6406e608d2adab6ff568ab0e
tehShuker <shuker.96@gmail.com>
2021-11-03 14:24:09 +0300

    fix

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,13 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DATABASE_URL + (process.env.NODE_ENV ? '?ssl=true' : ''), {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+          require: process.env.NODE_ENV ? true : false,
+          rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+        }
+    },
     logging: false
 })
 
